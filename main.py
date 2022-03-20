f5ce357dfa3488fb997f706cdd541ff41979acd7
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-03-20 18:40:37 +0700

    add & clean example scripts

--- main.py
+++ main.py
@@ -3,5 +3,17 @@
 
 from src.jrti import JRTI
 
+# jrti = JRTI()
+# jrti.run("scripts/hello-world.jrti")
+
+# jrti = JRTI()
+# jrti.run("scripts/string-number.jrti")
+
+# jrti = JRTI()
+# jrti.run("scripts/condition.jrti")
+
+# jrti = JRTI()
+# jrti.run("scripts/loop.jrti")
+
 jrti = JRTI()
-jrti.run("scripts/hello-world.jrti")
+jrti.run("scripts/function.jrti")
