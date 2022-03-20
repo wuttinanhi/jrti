b13d46036b073fcf564d1571133959955030b756
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-03-20 17:32:15 +0700

    turn util into class

--- src/util.py
+++ src/util.py
@@ -3,26 +3,28 @@
 from src.variable import Variable
 
 
-def is_number(value: any):
-    try:
-        float(value)
-    except ValueError:
-        return False
-    return True
+class Util:
+    @staticmethod
+    def is_number(value: any):
+        try:
+            float(value)
+        except ValueError:
+            return False
+        return True
 
+    @staticmethod
+    def parse_number(value: str):
+        try:
+            return int(value)
+        except ValueError:
+            return float(value)
 
-def parse_number(value: str):
-    try:
-        return int(value)
-    except ValueError:
-        return float(value)
-
-
-def parse_value(value: any):
-    if str(value).startswith("$") is True:
-        return Variable(value)
-    if str(value).startswith('"') and str(value).endswith('"') is True:
-        return str(value)[1:-1]
-    if is_number(value) is True:
-        return parse_number(value)
-    return str(value)
+    @staticmethod
+    def parse_value(value: any):
+        if str(value).startswith("$") is True:
+            return Variable(value)
+        if str(value).startswith('"') and str(value).endswith('"') is True:
+            return str(value)[1:-1]
+        if Util.is_number(value) is True:
+            return Util.parse_number(value)
+        return str(value)
--- src/value.py
+++ src/value.py
@@ -1,14 +1,14 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
 
 
-from src.util import parse_value
+from src.util import Util
 from src.variable import Variable
 from src.variable_storage import VariableStorage
 
 
 class Value:
     def __init__(self, value: any) -> None:
-        self.__value = parse_value(value)
+        self.__value = Util.parse_value(value)
 
     def get(self, variable_storage: VariableStorage):
         if isinstance(self.__value, Variable):
