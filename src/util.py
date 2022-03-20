31a20f20651d6077289f953790cc097169bf4e52
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-03-20 16:29:28 +0700

    better number check for parse_value

--- src/util.py
+++ src/util.py
@@ -3,6 +3,14 @@
 from src.variable import Variable
 
 
+def is_number(value: any):
+    try:
+        float(value)
+    except ValueError:
+        return False
+    return True
+
+
 def parse_number(value: str):
     try:
         return int(value)
@@ -15,6 +23,6 @@ def parse_value(value: any):
         return Variable(value)
     if str(value).startswith('"') is True:
         return str(value)[1:-1]
-    if str(value).isdigit() is False:
-        return str(value)
-    return parse_number(value)
+    if is_number(value) is True:
+        return parse_number(value)
+    return str(value)
